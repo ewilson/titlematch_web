fd1b17247ca6c73f4e55a93c630e4bf3bb01dc86
Eric Wilson <e.wilson.1@elsevier.com>
2015-01-20 19:50:17 -0500

    redone landing page, added archive page

--- app/router.js
+++ app/router.js
@@ -14,6 +14,7 @@ Router.map(function () {
       this.route('standings');
     });
     this.route('complete', {path: '/complete/:tournament_id'});
+    this.route("archive");
   });
   this.route('players', function(){
     this.route('new');
