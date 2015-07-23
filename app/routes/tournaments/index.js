d01c6c4fb84d1e07cef327c93ba3bee2b4ea10f1
Eric Wilson <wilson.eric.n@gmail.com>
2015-07-23 06:01:18 -0400

    Removed some deprecation warnings

--- app/routes/tournaments/index.js
+++ app/routes/tournaments/index.js
@@ -2,8 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     model: function() {
-        var newTournaments  = this.store.find('tournament', {state: 0});
-        var activeTournaments  = this.store.find('tournament', {state: 1});
+        var newTournaments  = this.store.query('tournament', {state: 0});
+        var activeTournaments  = this.store.query('tournament', {state: 1});
         return {newTournaments: newTournaments, activeTournaments: activeTournaments};
     }
 });
--- app/routes/tournaments/setup.js
+++ app/routes/tournaments/setup.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   beforeModel: function() {
     var that = this;
-    var players = this.store.find('player');
+    var players = this.store.findAll('player');
     players.then(function() {
       that.controllerFor('players').set('model',players);
     });
