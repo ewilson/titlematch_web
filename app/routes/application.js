cd48e327c70264b5956982c4b8e3fd6af08addb3
Eric Wilson <wilson.eric.n@gmail.com>
2015-11-26 20:35:05 -0500

    added forgotten file

---
+++ app/routes/application.js
@@ -0,0 +1,9 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model() {
+      var newTournaments  = this.store.query('tournament', {state: 0});
+      var activeTournaments  = this.store.query('tournament', {state: 1});
+      return {newTournaments: newTournaments, activeTournaments: activeTournaments};
+  }
+});
