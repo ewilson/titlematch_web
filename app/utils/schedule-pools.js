56700c6fe07122fdb9fac2c8a5d9262ff1e98471
Eric Wilson <wilson.eric.n@gmail.com>
2015-11-28 09:17:42 -0500

    refactor pool logic 2

--- app/utils/schedule-pools.js
+++ app/utils/schedule-pools.js
@@ -1,23 +1,9 @@
 import roundRobin from 'titlematch-web/utils/round-robin';
 
 export default function schedulePools(competitors, poolNames) {
-  // pools will have keys being poolNames, values being lists of competitors
   var pools = initPools(poolNames);
-  for (var i = 0; i < competitors.length; i++) {
-    var poolName = poolNames[i % poolNames.length];
-    pools[poolName].push(competitors[i]);
-  }
-
-  // matches will be a list of objects with keys 'home', 'away', and 'pool'
-  var matches = [];
-  for (i = 0; i < poolNames.length; i++) {
-    var poolMatches = roundRobin(pools[poolNames[i]]);
-    for (var j = 0; j < poolMatches.length; j++) {
-      poolMatches[j]['pool'] = poolNames[i];
-    }
-    matches = matches.concat(poolMatches);
-  }
-  return matches;
+  populatePools(competitors, pools, poolNames);
+  return buildMatches(pools, poolNames);
 }
 
 // pools will have keys being poolNames, values being lists of competitors
@@ -27,4 +13,25 @@ var initPools = function(names) {
     pools[names[i]] = [];
   }
   return pools;
+};
+
+var populatePools = function(competitors, pools, poolNames) {
+  for (var i = 0; i < competitors.length; i++) {
+    var poolName = poolNames[i % poolNames.length];
+    pools[poolName].push(competitors[i]);
+  }
+};
+
+// matches will be a list of objects with keys 'home', 'away', and 'pool'
+var buildMatches = function(pools, poolNames) {
+  var matches = [];
+  for (var i = 0; i < poolNames.length; i++) {
+    var poolMatches = roundRobin(pools[poolNames[i]]);
+    // adding pool name to each match
+    for (var j = 0; j < poolMatches.length; j++) {
+      poolMatches[j]['pool'] = poolNames[i];
+    }
+    matches = matches.concat(poolMatches);
+  }
+  return matches;
 };
